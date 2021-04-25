df1c06abe34833d35b284e2afad092d11b3fd2fc
uccs <1500846601@qq.com>
2021-04-25 09:57:05 +0800

    全局 state 的读写

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, {useContext, useState} from "react";
+import './App.css';
+
+const Context = React.createContext(null)
+
+const App = () => {
+  const [appState, setAppState] = useState({
+    user: {name: "uccs"}
+  })
+  const contextValue = {appState, setAppState}
+  return <Context.Provider value={contextValue}>
+    <div className="wrapper">
+      <div>
+        <Title/>
+        <Content/>
+        <Input/>
+      </div>
+    </div>
+  </Context.Provider>
+}
+
+
+const Title = () => {
+  return <div className="title">手写 redux</div>
+}
+
+const Content = () => {
+  const {appState} = useContext(Context)
+  return <div className="content">大家好，我是: {appState.user.name}</div>
+}
+
+const Input = () => {
+  const {appState, setAppState} = useContext(Context)
+  const onChange = (e) => {
+    appState.user.name = e.target.value
+    setAppState(appState)
+  }
+  return <div className="input">
+    <div className="label">请输入：</div>
+    <input type="text" onChange={onChange} value={appState.user.name}/>
+  </div>
+}
+
+export default App;
